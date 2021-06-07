1454d4bd3df1461f59e31d52aae20ec0b0598b92
thanhnv <nguyenthanh2303@gmail.com>
2021-06-07 08:11:40 +0700

    add: append skeleton on circulating asset and loan ratio extractor

--- jobs/extractor/circulating_asset_extractor.py
+++ jobs/extractor/circulating_asset_extractor.py
@@ -5,4 +5,19 @@ from jobs.extractor.extractor import Extractor
 class CirculatingAssetExtractor(Extractor):
 
     def extract(self, wallet_data):
+        wallet_address = wallet_data.get("address")
+        wallet_credit = self.database.get_wallet_credit(wallet_address)
+        if not wallet_credit:
+            return
+        self._supply_on_total_asset(wallet_data, wallet_credit)
+        self._interest_on_supply(wallet_data, wallet_credit)
+
+        # x41
+
+    def _supply_on_total_asset(self, wallet_data, wallet_credit):
+        pass
+
+        # x42
+
+    def _interest_on_supply(self, wallet_data, wallet_credit):
         pass
--- jobs/extractor/loan_ratio_extractor.py
+++ jobs/extractor/loan_ratio_extractor.py
@@ -1,8 +1,20 @@
-from database.database import Database
 from jobs.extractor.extractor import Extractor
 
 
 class LoanRatioExtractor(Extractor):
 
     def extract(self, wallet_data):
+        wallet_address = wallet_data.get("address")
+        wallet_credit = self.database.get_wallet_credit(wallet_address)
+        if not wallet_credit:
+            return
+        self._borrow_on_total_asset(wallet_data, wallet_credit)
+        self._borrow_on_supply(wallet_data, wallet_credit)
+
+    # x41
+    def _borrow_on_total_asset(self, wallet_data, wallet_credit):
+        pass
+
+    # x42
+    def _borrow_on_supply(self, wallet_data, wallet_credit):
         pass
